ef0029932cfbcb4c378c12c4f016e0e5f30f702f
DanielJ <danielwjameson@yahoo.com>
2018-09-17 21:28:30 -0500

    Cleanup

--- js/index.js
+++ js/index.js
@@ -2,42 +2,31 @@ const sql = require("mssql"); // https://www.npmjs.com/package/mssql
 
 console.log("This is a test");
 
+let error = function(err) {
+	console.log("ERROR: " + err);
+};
+
 const config = {
     user: "umkc_test",
     password: "abc123",
-    server: "127.0.0.1", // You can use 'localhost\\instance' to connect to named instance
+    server: "127.0.0.1",
     database: "GroupProjectDB",
     options: {
-        encrypt: false, // Use this if you're on Windows Azure
+        encrypt: true,
     }
 };
 
 sql.connect(config).then(pool => {
-    // Query
-    
 	console.log("1");
-	
     return pool.request()
 		.query("SELECT * from TestTable");
 }).then(result => {
-    console.dir(result);
-    
-    // Stored procedure
 	console.log("2");
-    
-    //return pool.request()
-    //.input('input_parameter', sql.Int, value)
-    //.output('output_parameter', sql.VarChar(50))
-	//.execute('procedure_name');
-}).then(result => {
-	console.dir(result);
-	console.log("3");
+	console.log(result);
 }).catch(err => {
-	// ... error checks
-	console.log("err: " + err);
+	error(err);
 });
  
 sql.on("error", err => {
-	// ... error handler
-	console.log("MSSQL ERROR");
+	error(err);
 });
