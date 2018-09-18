a28c35ae309aba676a4d01d9c22fe2e61c6b6131
DanielJ <danielwjameson@yahoo.com>
2018-09-17 21:57:26 -0500

    Simplify mssql stuff

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const sql = require("mssql"); // https://www.npmjs.com/package/mssql
+const $ = require("jquery");
 
 console.log("This is a test");
 
@@ -6,27 +7,21 @@ let error = function(err) {
 	console.log("ERROR: " + err);
 };
 
-const config = {
+let pool = new sql.ConnectionPool({
     user: "umkc_test",
     password: "abc123",
     server: "127.0.0.1",
     database: "GroupProjectDB",
-    options: {
-        encrypt: true,
-    }
+});
+
+let main = function() {
+	pool.query("SELECT * FROM TestTable").then(res => {
+		console.log(res);
+	});
 };
 
-sql.connect(config).then(pool => {
-	console.log("1");
-    return pool.request()
-		.query("SELECT * from TestTable");
-}).then(result => {
-	console.log("2");
-	console.log(result);
+pool.connect().then(() => {
+	main();
 }).catch(err => {
-	error(err);
-});
- 
-sql.on("error", err => {
 	error(err);
 });
