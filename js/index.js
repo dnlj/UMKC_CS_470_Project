0084aeb8a49d84056604e86b58de9bb4a3d5958f
DanielJ <danielwjameson@yahoo.com>
2018-11-28 22:45:03 -0600

    Move button creation out of html

--- js/index.js
+++ js/index.js
@@ -2,7 +2,28 @@ const sql = require("mssql"); // https://www.npmjs.com/package/mssql
 const fs = require("fs");
 const $ = require("jquery");
 
-console.log("index.js");
+const pages = [
+	{
+		label: "Vendors",
+		url: "../html/example.html",
+	},
+	{
+		label: "Warehouse",
+		url: "../html/example.html",
+	},
+	{
+		label: "Products",
+		url: "../html/example.html",
+	},
+	{
+		label: "Employees",
+		url: "../html/example.html",
+	},
+	{
+		label: "Stores",
+		url: "../html/example.html",
+	},
+];
 
 let error = function(err) {
 	console.log("ERROR: " + err);
@@ -15,19 +36,37 @@ let pool = new sql.ConnectionPool({
 	database: "GroupProjectDB",
 });
 
+const clearPage = function() {
+	$("main").empty();
+	
+	for (let i = 0; i < pages.length; ++i) {
+		pages[i].button.removeClass("selected");
+	}
+}
+
+const loadPage = function(page) {
+	clearPage();
+	$("main").load(page.url);
+	page.button.addClass("selected");
+};
+
 let main = function() {
 	$.ajaxPrefilter(function(op) {
 		op.async = true;
 	});
 	
-	/*$("#page_select").change(function() {
-		$("main").load("../html/" + $(this).val() +".html");
-	});*/
-	
-	$("main").load("../html/example.html");
+	for (let i = 0; i < pages.length; ++i) {
+		const p = pages[i];
+		p.button = $(`<input class="nav_button" type="button" value="${p.label}">`)
+			.appendTo("html > body > nav");
+			
+		p.button.on("click", function() {
+			console.log(p);
+			loadPage(p);
+		});
+	}
 };
 
-// TEMP
 main();
 
 /*
